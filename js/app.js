297c5b43aa2b87abd33180fb1108ec66e28ad42b
Peter <p.sivak91@gmail.com>
2022-01-12 18:55:49 -0800

    D

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 
+  
+  /******************  GreenSock *****************/
+
+  gsap.from('.dropdown', { duration: 3, y: '-300px', ease: "circ.out", y: -500 })
+
+  gsap.from('.name', {duration:4, x: '-900px', ease: "elastic.out(1, 0.4)", x: -900 })
+
+  gsap.from(".imgtitle", {opacity: 0, duration: 7, delay: 3.7})
+
+
+
+
+
 
 
       //DRESY & LOGO
