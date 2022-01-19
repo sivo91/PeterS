1a1688598046a31b923658457469fabb0732d67f
Peter <p.sivak91@gmail.com>
2022-01-19 12:28:52 -0800

    D

--- js/intro.js
+++ js/intro.js
@@ -1,9 +1,9 @@
 
    /******************  GreenSock *****************/
 
-  gsap.from('.dropdown', { duration: 3, y: '-300px', ease: "circ.out", y: -500 })
+  gsap.from('.dropdown', { duration: 2, y: '-300px', ease: "circ.out", y: -500 })
 
-  gsap.from('.name', {duration:4, x: '-900px', ease: "elastic.out(1, 0.4)", x: -900 })
+  /* gsap.from('.name', {duration:4, x: '-90px', ease: "elastic.out(1, 4)", x: -90 }) */
 
   gsap.from(".imgtitle", {opacity: 0, duration: 9, delay: 0})
 
