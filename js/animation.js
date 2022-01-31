bb47070a908f85aa164ed39027cf52a183978046
Peter <p.sivak91@gmail.com>
2022-01-30 20:21:39 -0800

    adding fire and snowing

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,8 @@
 
 
-const play = document.querySelector('.passing');
+   // PRIHRAVOCKY
+
+   //const play = document.querySelector('.passing');
 
         const svgContainer = document.getElementById('svg');
         const animItem = bodymovin.loadAnimation({
@@ -11,45 +13,39 @@ const play = document.querySelector('.passing');
             path: 'https://assets3.lottiefiles.com/packages/lf20_GWWy4D.json'
         });
 
-        play.addEventListener('mouseover', () => {
+        /* play.addEventListener('mouseover', () => {
             svgContainer.classList.remove('hide');
             animItem.goToAndPlay(0,true);
-        })
+        }) */
 
         animItem.addEventListener('complete', () => {
             svgContainer.classList.add('hide');
-            
         })
 
+    
 
 
+ // TN LOGO 
 
-      /*   const play1 = document.querySelector('.passing1');
-
-        const svgContainer1 = document.getElementById('svg1');
-        const animItem1 = bodymovin.loadAnimation({
-            wrapper: svgContainer1,
+       const svgBox = document.querySelector('.fire')
+       const anim = bodymovin.loadAnimation({
+            wrapper: svgBox,
             animType: 'svg',
             loop: true,
             autoplay: true,
-            path: 'https://assets9.lottiefiles.com/packages/lf20_rmlyntkm.json'
+            path: 'https://assets1.lottiefiles.com/packages/lf20_no9qrf5p.json'
         });
-
-        play1.addEventListener('mouseover', () => {
-            svgContainer1.classList.remove('hide');
-            animItem.goToAndPlay(0,true);
-        })
-
-        animItem.addEventListener('complete', () => {
-            svgContainer1.classList.add('hide');
-            
-        }) */
-
-
-
-
  
+// AK LOGO SNOW
 
+     const svgSnow = document.querySelector('.snow')
+     const anime = bodymovin.loadAnimation({
+            wrapper: svgSnow,
+            animType: 'svg',
+            loop: true,
+            autoplay: true,
+            path: 'https://assets1.lottiefiles.com/packages/lf20_kZx06J.json'
+        });
 
 
 
