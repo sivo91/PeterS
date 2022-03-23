8451b33e42a4c54ccfe30495c882e10c872e5adb
Peter <p.sivak91@gmail.com>
2022-03-22 17:21:44 -0700

    trading cards

---
+++ js/karticky.js
@@ -0,0 +1,31 @@
+
+const cards = document.querySelectorAll('.karticka')
+
+  // SINGLE CLICK - TURN CARD
+ cards.forEach(function(card) {
+   card.addEventListener('click', flipCard)
+ })
+
+ let front, back
+ let turnedCard = false
+
+
+function flipCard() {
+  //console.log(this)
+  this.classList.toggle('turn')
+  let audioEffect = new Audio('../sound/click.mp3')
+  audioEffect.play()
+  audioEffect.volume = .5
+
+  if(!turnedCard) {
+    // prvy click
+     turnedCard = true
+     
+     front = this 
+  } else {
+    // druhy click
+    turnedCard = false
+    back = this
+  }
+}
+
